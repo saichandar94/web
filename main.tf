6b0fd9668e0b54221e065d212ee98e823dc08227
saichandar94 <47038974+saichandar94@users.noreply.github.com>
2019-02-13 10:12:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
+  AWS_ACCESS_KEY_ID = "AKIAJWYASN57BFPUEMOA"
+  AWS_SECRET_ACCESS_KEY = "R7LJS78rWiLDAmaiait+HIennzaB3BOlbSRQIMgj"
 }
 resource "aws_instance" "example" {
 ami = "ami-0ad42f4f66f6c1cc9"
